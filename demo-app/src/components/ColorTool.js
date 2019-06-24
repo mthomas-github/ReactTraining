f56728f40046632444f20ed21c56380273e4f1c2
Michael Thomas <mthomas2270@gmail.com>
2019-06-24 16:44:35 -0700

    start of exercise 5

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 import { useForm } from '../hooks/useForm';
 
 export const ColorTool = (props) => {
 
-    const [ colorForm, change ] = useForm({
-        newColor: ''
-    });
-    return <>
-        <header>
-            <h1>Color Tool</h1>
-        </header>
-        <ul>
-            {props.colors.map((color, index) => <li key={index}>{color}</li>)}
-        </ul>
-        <form>
-            <div>
-                <label htmlFor="new-color-input">New Color:</label>
-                <input type="text" id="new-color-input" name="newColor" value={colorForm.newColor} onChange={change} />
-            </div>
-        </form>
-    </>;
+  const [ colorForm, change ] = useForm({
+    newColor: '',
+    newHexCode: '',
+  })
+
+  return <>
+    <header>
+      <h1>Color Tool</h1>
+    </header>
+    <ul>
+      {props.colors.map((color, index) => <li key={index}>{color}</li>)}
+    </ul>
+    <form>
+      <div>
+        <label htmlFor="new-color-input">New Color:</label>
+        <input type="text" id="new-color-input" name="newColor" value={colorForm.newColor} onChange={change} />
+      </div>
+      <div>
+        <label htmlFor="new-hexcode-input">New HexCode:</label>
+        <input type="text" id="new-hexcode-input" name="newHexCode" value={colorForm.newHexCode} onChange={change} />
+      </div>
+    </form>
+  </>;
+
 };
--- demo-app/src/hooks/useForm.js
+++ demo-app/src/hooks/useForm.js
@@ -1,12 +1,18 @@
-export const useForm = (initialState) => {
-    const [form, setForm ] = useState(initialState);
+import { useState } from 'react';
 
-    const change = (e) => {
-        setForm({
-            ...form,
-            [ e.target.name ]: e.target.type === 'number'
-            ? Number(e.target.value)
-            : e.target.value
-        });
-    };
-}
+export const useForm = (initialState) => {
+
+  const [ form, setForm ] = useState(initialState);
+
+  const change = (e) => {
+    setForm({
+      ...form,
+      [ e.target.name ]: e.target.type === 'number'
+        ? Number(e.target.value)
+        : e.target.value,
+    });
+  };
+
+  // form data, change function, reset form function
+  return [ form, change, () => setForm(initialState) ];
+};
