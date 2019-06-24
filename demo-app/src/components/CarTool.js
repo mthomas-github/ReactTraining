41c3ef6283896f054ba070109aceae02c6f6c511
Michael Thomas <mthomas2270@gmail.com>
2019-06-24 13:20:35 -0700

    format fixing

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export const CarTool = () => {
     // JSX Syntax
-    return <><header>
-        <h1>Car Tool</h1>
-    </header>
+    return <>
+        <header>
+            <h1>Car Tool</h1>
+        </header>
         <table>
             <tr>
                 <th>Id</th>
