2b6e28a78352fab11b6bed9e91848974df5e5091
Michael Thomas <mthomas2270@gmail.com>
2019-06-24 13:23:25 -0700

    fixed console errors with table

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -7,30 +7,34 @@ export const CarTool = () => {
             <h1>Car Tool</h1>
         </header>
         <table>
-            <tr>
-                <th>Id</th>
-                <th>Make</th>
-                <th>Model</th>
-                <th>Year</th>
-                <th>Color</th>
-                <th>Price</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Telsa</td>
-                <td>Model S</td>
-                <td>2019</td>
-                <td>Black</td>
-                <td>$165,000</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Telsa</td>
-                <td>Model 3</td>
-                <td>2019</td>
-                <td>White</td>
-                <td>$60,000</td>
-            </tr>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Color</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Telsa</td>
+                    <td>Model S</td>
+                    <td>2019</td>
+                    <td>Black</td>
+                    <td>$165,000</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Telsa</td>
+                    <td>Model 3</td>
+                    <td>2019</td>
+                    <td>White</td>
+                    <td>$60,000</td>
+                </tr>
+            </tbody>
         </table>
     </>;
 };
