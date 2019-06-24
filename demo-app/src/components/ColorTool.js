1f2a6f9cd1b6a81e6191ba9ffcf71d86a5151ac2
Michael Thomas <mthomas2270@gmail.com>
2019-06-24 15:55:37 -0700

    End of Exercise 4

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -11,5 +11,11 @@ export const ColorTool = (props) => {
         <ul>
             {props.colors.map((color, index) => <li key={index}>{color}</li>)}
         </ul>
+        <form>
+            <div>
+                <label htmlFor="new-color-input">New Color:</label>
+                <input type="text" id="new-color-input" value={} onChange={} />
+            </div>
+        </form>
     </>;
 };
