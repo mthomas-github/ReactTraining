0ffe148373b8573100389b870f1329b47f970134
Michael Thomas <mthomas2270@gmail.com>
2019-06-24 13:46:17 -0700

    dynamic list using map fuction

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -3,19 +3,17 @@ import React from 'react';
 
 
 export const ColorTool = () => {
+
+    const colors = [
+        'purple', 'blue', 'green', 'red', 'pink', 'orange', 'black', 'yellow'
+    ];
+
     return <>
         <header>
             <h1>Color Tool</h1>
         </header>
         <ul>
-            <li>purple</li>
-            <li>blue</li>
-            <li>green</li>
-            <li>red</li>
-            <li>pink</li>
-            <li>orange</li>
-            <li>black</li>
-            <li>yellow</li>
+            {colors.map((color, index) => <li key={index}>{color}</li>)}
         </ul>
     </>;
 };
