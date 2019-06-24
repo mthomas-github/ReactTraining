dfa9e340d3728c3419e97849edcf5a502eb3352e
Michael Thomas <mthomas2270@gmail.com>
2019-06-24 13:06:24 -0700

    Created Unsorted list

---
+++ demo-app/src/components/ColorTool.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+export const ColorTool = () => {
+    return <>
+        <header>
+            <h1>Color Tool</h1>
+        </header>
+        <ul>
+            <li>purple</li>
+            <li>blue</li>
+            <li>green</li>
+            <li>red</li>
+            <li>pink</li>
+            <li>orange</li>
+            <li>black</li>
+            <li>yellow</li>
+        </ul>
+    </>;
+};
