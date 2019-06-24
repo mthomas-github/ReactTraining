79181d74ee1209431ba2fd17827ada9f217398c1
Michael Thomas <mthomas2270@gmail.com>
2019-06-24 13:17:12 -0700

    End of exercise 2

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -2,7 +2,34 @@ import React from "react";
 
 export const CarTool = () => {
     // JSX Syntax
-    return <header>
+    return <><header>
         <h1>Car Tool</h1>
-    </header>;
+    </header>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Make</th>
+                <th>Model</th>
+                <th>Year</th>
+                <th>Color</th>
+                <th>Price</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Telsa</td>
+                <td>Model S</td>
+                <td>2019</td>
+                <td>Black</td>
+                <td>$165,000</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Telsa</td>
+                <td>Model 3</td>
+                <td>2019</td>
+                <td>White</td>
+                <td>$60,000</td>
+            </tr>
+        </table>
+    </>;
 };
