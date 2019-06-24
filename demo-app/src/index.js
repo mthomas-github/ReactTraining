7911301a8ce35c14435da066d7d90415ba46ea19
Michael Thomas <Michael.Thomas@teletracnavman.com>
2019-06-24 10:46:34 -0700

    Showing H1 tag HelloWorld in div class root

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,3 +1,11 @@
-import { React } from "react";
-import { ReactDom } from 'react-dom';
+import React from "react";
+import ReactDOM from 'react-dom';
 
+const HelloWorld = () => {
+    return React.createElement('h1', null, 'Hello World!')
+};
+
+ReactDOM.render(
+    React.createElement(HelloWorld),
+    document.querySelector('#root'),
+);
