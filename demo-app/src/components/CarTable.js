a6e159619b2a367deaa53ddf433f17cfe68489a7
Michael Thomas <mthomas2270@gmail.com>
2019-06-25 11:36:47 -0700

    end of exericse 7

--- demo-app/src/components/CarTable.js
+++ demo-app/src/components/CarTable.js
@@ -1,29 +1,30 @@
 import React from "react";
 
-
-export const CarTable = (props) => {
-    return  <table>
-    <thead>
-      <tr>
-        <th>Id</th>
-        <th>Make</th>
-        <th>Model</th>
-        <th>Year</th>
-        <th>Color</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.cars.map(car => (
-        <tr key={car.id}>
-          <td>{car.id}</td>
-          <td>{car.make}</td>
-          <td>{car.model}</td>
-          <td>{car.year}</td>
-          <td>{car.color}</td>
-          <td>{car.price}</td>
+export const CarTable = props => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Year</th>
+          <th>Color</th>
+          <th>Price</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-}
+      </thead>
+      <tbody>
+        {props.cars.map(car => (
+          <tr key={car.id}>
+            <td>{car.id}</td>
+            <td>{car.make}</td>
+            <td>{car.model}</td>
+            <td>{car.year}</td>
+            <td>{car.color}</td>
+            <td>{car.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
