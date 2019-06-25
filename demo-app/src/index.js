593db5b974218268607741affd548c2986fd125c
Michael Thomas <mthomas2270@gmail.com>
2019-06-25 09:25:06 -0700

    clean up formating

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -4,35 +4,21 @@ import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
 
 const colorsList = [
-    'purple', 'blue', 'green', 'red', 
+    'purple', 'blue', 'green', 'red',
     'pink', 'orange', 'black', 'yellow'
 ];
 
 const carsList = [
-    {
-        id: 'Telsa1',
-        make: 'Telsa',
-        model: 'Model S',
-        year: 2019,
-        color: 'Black',
-        price: 165000
-    },
-    {
-        id: 'Telsa2',
-        make: 'Telsa',
-        model: 'Model 3',
-        year: 2019,
-        color: 'White',
-        price: 65000
-    }
+    { id: 'Telsa1', make: 'Telsa', model: 'Model S', year: 2019, color: 'Black', price: 165000 },
+    { id: 'Telsa2', make: 'Telsa', model: 'Model 3', year: 2019, color: 'White', price: 65000 }
 ];
 
 
 ReactDOM.render(
     // <> == <React.Fragment>
     <>
-    <ColorTool colors={colorsList} />
-    <CarTool cars={carsList} />
+        <ColorTool colors={colorsList} />
+        <CarTool cars={carsList} />
     </>,
     document.querySelector('#root'),
 );
