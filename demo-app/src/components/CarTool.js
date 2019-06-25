fc15a8c7debf86802ec3ca0a01e5aea9b8f864b6
Michael Thomas <mthomas2270@gmail.com>
2019-06-25 09:43:28 -0700

    more fomatting of code on CarTool.js

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -45,23 +45,28 @@ export const CarTool = (props) => {
         <form>
             <div>
                 <label htmlFor="make-input">Make:</label>
-                <input type="text" id="make-input" name="newMake" value={carForm.newMake} onChange={change} />
+                <input type="text" id="make-input" name="newMake"
+                    value={carForm.newMake} onChange={change} />
             </div>
             <div>
                 <label htmlFor="model-input">Model:</label>
-                <input type="text" id="model-input" name="newModel" value={carForm.newModel} onChange={change} />
+                <input type="text" id="model-input" name="newModel"
+                    value={carForm.newModel} onChange={change} />
             </div>
             <div>
                 <label htmlFor="year-input">Year:</label>
-                <input type="number" id="year-input" name="newYear" value={carForm.newYear} onChange={change} />
+                <input type="number" id="year-input" name="newYear"
+                    value={carForm.newYear} onChange={change} />
             </div>
             <div>
                 <label htmlFor="color-input">Color:</label>
-                <input type="text" id="color-input" name="newColor" value={carForm.newColor} onChange={change} />
+                <input type="text" id="color-input" name="newColor"
+                    value={carForm.newColor} onChange={change} />
             </div>
             <div>
                 <label htmlFor="price-input">Price:</label>
-                <input type="number" id="price-input" name="newPrice" value={carForm.newPrice} onChange={change} />
+                <input type="number" id="price-input" name="newPrice"
+                    value={carForm.newPrice} onChange={change} />
             </div>
         </form>
     </>;
